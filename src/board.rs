78a8a2086de64d6b1767a46bf973940478090565
Stephen Newman <steve@newman.digital>
2021-05-16 21:45:18 +0100

    Pull from source

--- src/board.rs
+++ src/board.rs
@@ -10,24 +10,15 @@ pub struct GamePiece {
     pub crowned: bool,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub struct Coordinate(pub usize, pub usize);
-
-#[derive(Debug, Clone, PartialEq, Copy)]
-pub struct Move {
-    pub from: Coordinate,
-    pub to: Coordinate,
-}
-
 impl GamePiece {
-    pub fn new(color: PieceColor) -> Self {
+    pub fn new(color: PieceColor) -> GamePiece {
         GamePiece {
             color,
             crowned: false,
         }
     }
 
-    pub fn crowned(p: GamePiece) -> Self {
+    pub fn crowned(p: GamePiece) -> GamePiece {
         GamePiece {
             color: p.color,
             crowned: true,
@@ -35,12 +26,18 @@ impl GamePiece {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Coordinate(pub usize, pub usize);
+
 impl Coordinate {
+
     pub fn on_board(self) -> bool {
-        let Coordinate(x, y) = self;
+        let Coordinate(x, y) = self; // (1)
         x <= 7 && y <= 7
     }
 
+
+    // (2)
     pub fn jump_targets_from(&self) -> impl Iterator<Item = Coordinate> {
         let mut jumps = Vec::new();
         let Coordinate(x, y) = *self;
@@ -55,10 +52,10 @@ impl Coordinate {
         if x >= 2 {
             jumps.push(Coordinate(x - 2, y + 2));
         }
-
         jumps.into_iter()
     }
 
+    // (3)
     pub fn move_targets_from(&self) -> impl Iterator<Item = Coordinate> {
         let mut moves = Vec::new();
         let Coordinate(x, y) = *self;
@@ -66,20 +63,24 @@ impl Coordinate {
             moves.push(Coordinate(x - 1, y + 1));
         }
         moves.push(Coordinate(x + 1, y + 1));
-
         if y >= 1 {
             moves.push(Coordinate(x + 1, y - 1));
         }
-        if x >= 1 && y >=1 {
+        if x >= 1 && y >= 1 {
             moves.push(Coordinate(x - 1, y - 1));
         }
-
         moves.into_iter()
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Move {
+    pub from: Coordinate,
+    pub to: Coordinate,
+}
+
 impl Move {
-    pub fn new(from: (usize, usize), to: (usize, usize)) -> Self {
+    pub fn new(from: (usize, usize), to: (usize, usize)) -> Move {
         Move {
             from: Coordinate(from.0, from.1),
             to: Coordinate(to.0, to.1),
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-mod board;
-mod game;
+extern crate mut_static;
+
+extern "C" {
+    fn notify_piecemoved(fromX: i32, fromY: i32,
+                         toX: i32, toY: i32);
+    fn notify_piececrowned(x: i32, y: i32);
+}
 
 #[macro_use]
 extern crate lazy_static;
@@ -8,32 +13,9 @@ use board::{Coordinate, GamePiece, Move, PieceColor};
 use game::GameEngine;
 use mut_static::MutStatic;
 
-const PIECEFLAG_BLACK: u8 = 1;
-const PIECEFLAG_WHITE: u8 = 2;
-const PIECEFLAG_CROWN: u8 = 4;
-
 lazy_static! {
     pub static ref GAME_ENGINE: MutStatic<GameEngine> =
-    MutStatic::from(GameEngine::new());
-}
-
-#[no_mangle]
-pub extern "C" fn get_piece(x: i32, y: i32) -> i32 {
-    let engine = GAME_ENGINE.read().unwrap();
-
-    let piece = engine.get_piece(Coordinate(x as usize, y as usize));
-    match piece {
-        Ok(Some(p)) => p.into(),
-        Ok(None) => -1,
-        Err(_) => - 1,
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn get_current_turn() -> i32 {
-    let engine = GAME_ENGINE.read().unwrap();
-
-    GamePiece::new(engine.current_turn()).into()
+        { MutStatic::from(GameEngine::new()) };
 }
 
 #[no_mangle]
@@ -53,14 +35,32 @@ pub extern "C" fn move_piece(fx: i32, fy: i32, tx: i32, ty: i32) -> i32 {
             }
             1
         }
-        Err(_) => 0
+        Err(_) => 0,
     }
 }
 
-extern "C" {
-    fn notify_piecemoved(fromX: i32, fromY: i32, toX: i32, toY: i32);
-    fn notify_piececrowned(x: i32, y: i32);
+#[no_mangle]
+pub extern "C" fn get_piece(x: i32, y: i32) -> i32 {
+    let engine = GAME_ENGINE.read().unwrap();
+
+    let piece = engine.get_piece(Coordinate(x as usize, y as usize));
+    match piece {
+        Ok(Some(p)) => p.into(),
+        Ok(None) => -1,
+        Err(_) => -1,
+    }
 }
+
+#[no_mangle]
+pub extern "C" fn get_current_turn() -> i32 {
+    let engine = GAME_ENGINE.read().unwrap();
+
+    GamePiece::new(engine.current_turn()).into()
+}
+
+const PIECEFLAG_BLACK: u8 = 1;
+const PIECEFLAG_WHITE: u8 = 2;
+const PIECEFLAG_CROWN: u8 = 4;
 
 impl Into<i32> for GamePiece {
     fn into(self) -> i32 {
@@ -78,3 +78,6 @@ impl Into<i32> for GamePiece {
         val as i32
     }
 }
+
+mod board;
+mod game;
